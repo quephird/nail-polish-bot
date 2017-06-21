1bab086416e188147aa3eefbc0107858af9c44a2
danielle kefford <danielle_kefford@hotmail.com>
2017-06-20 21:47:02 -0400

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nail-polish-bot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/nail_polish_bot/core_test.clj
@@ -0,0 +1,7 @@
+(ns nail-polish-bot.core-test
+  (:require [clojure.test :refer :all]
+            [nail-polish-bot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
