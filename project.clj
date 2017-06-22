ac5af93a4f7f37ccb0c46bd05ba5c82aae268d71
danielle kefford <danielle_kefford@hotmail.com>
2017-06-22 01:56:51 -0400

    First cut at invoking POV-Ray from within Clojure.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [me.raynes/conch "0.8.0"]]
+  :main nail-polish-bot.core)
--- src/nail_polish_bot/core.clj
+++ src/nail_polish_bot/core.clj
@@ -1,6 +1,12 @@
-(ns nail-polish-bot.core)
+(ns nail-polish-bot.core
+  (:require [me.raynes.conch.low-level :as sh]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main []
+  (let [povray-bin  "povray"
+        povray-file "hello.pov"
+        povray-args (format "-d +I%s +Ohello.png +W800 +H600" povray-file)
+        process     (sh/proc povray-bin povray-args)
+        exit        (sh/exit-code process)]
+    (if (not (zero? exit))
+      (println "Uh oh, something happened")
+      (println "Yay! Image generated successfully"))))
