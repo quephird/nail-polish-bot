e18d6229a4266929109638ea0d24640162fcba8f
danielle kefford <danielle_kefford@hotmail.com>
2017-06-24 20:29:50 -0400

    Added TODOs

--- src/nail_polish_bot/core.clj
+++ src/nail_polish_bot/core.clj
@@ -6,10 +6,11 @@
 
 (defn post-status []
   (let [env-vars  (map env/env [:app-consumer-key
-                               :app-consumer-secret
-                               :user-access-token
-                               :user-access-token-secret])
+                                :app-consumer-secret
+                                :user-access-token
+                                :user-access-token-secret])
         bot-creds (apply oauth/make-oauth-creds env-vars)]
+    ; TODO: Need to check env-vars to see that it actually has something
     (api/statuses-update :oauth-creds bot-creds
                          :params {:status "Hiiiiii again Twitter!1!!"})))
 
@@ -19,6 +20,7 @@
         povray-args (format "-d +I%s +Ohello.png +W800 +H600" povray-file)
         process     (sh/proc povray-bin povray-args)
         exit        (sh/exit-code process)]
+    ; Need to make sure exit-code actually waits for proc to complete before returning
     (if (not (zero? exit))
       (println "Uh oh, something happened")
       (println "💅 Yay! Image generated successfully 💅"))))
