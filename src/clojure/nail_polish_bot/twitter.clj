9bdd29e83bd8d18d085b26bfe9276b95525e924e
quephird <danielle_kefford@hotmail.com>
2021-05-29 18:38:24 -0700

    Trying another thing.

--- src/clojure/nail_polish_bot/twitter.clj
+++ src/clojure/nail_polish_bot/twitter.clj
@@ -12,12 +12,15 @@
                                  :user-access-token
                                  :user-access-token-secret])
         bot-creds  (apply oauth/make-oauth-creds env-vars)
-        new-file   (req/file-body-part filename)
-        new-status (req/status-body-part status)
-        body       [new-file new-status]
-        api-params {:oauth-creds bot-creds
-                    :body        body}]
+        ;; new-file   (req/file-body-part filename)
+        ;; new-status (req/status-body-part status)
+        ;; body       [new-file new-status]
+        ;; api-params {:oauth-creds bot-creds
+        ;;             :body        body}
+        ]
     ; TODO: Need to check env-vars to see that it actually has something
     (try
-      (apply api/statuses-update-with-media api-params)
+      (api/statuses-update-with-media :oauth-creds bot-creds
+                                      :body [(req/file-body-part filename)
+                                             (req/status-body-part status)])
       (catch Exception e (println (.getMessage e))))))
